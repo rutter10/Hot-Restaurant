a860ae5b775e9c4376918ac016f27d8712e6dc30
Eric Shaw <ericshawmail@gmail.com>
2021-04-03 13:25:58 -0400

    set up express

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const reservation =[];
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'view.html')));
+
+app.get('/api/reservation', (req, res) => res.json(reservation));
+
+app.post("/api/reservation", (req, res) => {
+    const newReservation = req.body;
+    toLowercase();
+    reservation.push(newReservation.customerID);
+    res.json(newReservation);
+})
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
